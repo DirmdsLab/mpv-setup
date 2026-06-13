062aef7141af0f411ee99b55cfba30381a7e8d32
DirmdsLab <141147147+DirmdsLab@users.noreply.github.com>
2026-06-13 23:55:32 +0700

    sub

--- scripts/subtitle_seek_prev.lua
+++ scripts/subtitle_seek_prev.lua
@@ -6,5 +6,10 @@ local function seek_prev_sub()
     mp.command("sub-seek -1")
 end
 
--- keybind: n
+local function seek_next_sub()
+    mp.command("sub-seek 1")
+end
+
 mp.add_key_binding("n", "subtitle_seek_prev", seek_prev_sub)
+
+mp.add_key_binding("N", "subtitle_seek_next", seek_next_sub)
